615c24f409ef8bec5d52501fbc3b65a5d8acf647
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-08-16 09:48:42 -0400

    refactor: use relative paths

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
 import Lane_Lines as laneline
-import tensorflow as tf
 
 
 
 def get_lines():
-    lines = laneline.video("/Users/dylanmashini/PycharmProjects/Self Driving/video.mp4", True, True)
+    lines = laneline.video("./video.mp4", True, True)
     return  lines
 
 get_lines()
